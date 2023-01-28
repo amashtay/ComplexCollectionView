8f2fa1e83b52290566be352973fbb9af0028ba15
amashtay <a.tonkhonoev@gmail.com>
2023-01-28 12:44:10 +0600

    Initial Commit

---
+++ ComplexCollectionView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ComplexCollectionView
+//
+//  Created by Александр Тонхоноев on 28.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
